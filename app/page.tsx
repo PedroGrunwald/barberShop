882a9237d1519b4ee7e8fc6885b37afd2bb11863
Pedro Grunwald <pedro.grunwald.com>
2024-08-11 17:01:07 -0300

    feat: add Bookingitem

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
     <div>
       <Header />
+
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, xxx</h2>
         <p>xxxx, xx de agosto.</p>
@@ -26,6 +30,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit"> Confirmado </Badge>
+              <h3 className="font-semibold">Corte de cabelo xxx </h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage />
+                </Avatar>
+                <p className="text-sm">Barbearia XXX</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Mes XX</p>
+              <p className="text-2xl">dia XX</p>
+              <p className="text-sm">hora XX</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
